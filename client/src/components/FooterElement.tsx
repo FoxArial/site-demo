f028fe33ae36e45e89d21e8ca13d661615edfd79
Olena/Aria <savchuk.elenas@gmail.com>
2025-11-06 18:12:55 +0200

    bottom styling

--- client/src/components/FooterElement.tsx
+++ client/src/components/FooterElement.tsx
@@ -10,67 +10,71 @@ export default async function FooterElement() {
 
   const { description, infoColumns, links, logo, socialLogos, text } = footer;
   return (
-    <footer className="topFooter footer-style main-side-padding">
-      <div className="footer-columns">
+    <footer className="footer-style">
+      <div className="topFooter footer-columns main-side-padding">
         {infoColumns.map((item) => (
-          <div key={item.id}>
-            <p
-              className={`text-${item.title.color} textWeight-${item.title.fontWeight} text-smallTitle footer-columns-title`}
-            >
-              {item.title.label}
-            </p>
+          <div key={item.id} className=" footer-column">
+            <div className="inside">
+              <p
+                className={`text-${item.title.color} textWeight-${item.title.fontWeight} text-smallTitle footer-columns-title`}
+              >
+                {item.title.label}
+              </p>
 
-            {item.links.map((element) => (
-              <ul key={element.id}>
-                <li>
-                  <Link
-                    href={element.href}
-                    className={`text-${element.label?.color} textWeight-${element.label?.fontWeight} text-normal`}
-                  >
-                    {element.label?.label}
-                  </Link>
-                </li>
-              </ul>
-            ))}
+              {item.links.map((element) => (
+                <ul key={element.id}>
+                  <li className="column-li">
+                    <Link
+                      href={element.href}
+                      className={`text-${element.label?.color} textWeight-${element.label?.fontWeight} text-normal`}
+                    >
+                      {element.label?.label}
+                    </Link>
+                  </li>
+                </ul>
+              ))}
+            </div>
           </div>
         ))}
       </div>
-      <div className="middleFooter flex-central-space">
-        <Logo
-          footer={footer}
-          className={`text-${logo.label?.color} textWeight-${logo.label?.fontWeight} text-title`}
-        />
-        <div className="social flex-central-space">
-          <p
-            className={`text-${description.color} textWeight-${description.fontWeight} text-normal`}
-          >
-            {description.label}
-          </p>
-          <ul className="flex-central-space">
-            {socialLogos.map((element) => (
-              <li key={element.id}>
-                <Link href={element.href}>
-                  {element.image && (
-                    <StrapiImage
-                      src={element.image?.url}
-                      alt={element.label.label}
-                      width={25}
-                      height={25}
-                    />
-                  )}
-                </Link>
-              </li>
-            ))}
-          </ul>
+      <div className="middleFooter">
+        <div className="middleFooter-content flex-central-space">
+          <Logo
+            footer={footer}
+            className={`text-${logo.label?.color} textWeight-${logo.label?.fontWeight} text-title`}
+          />
+          <div className="social">
+            <p
+              className={`text-${description.color} textWeight-${description.fontWeight} text-normal`}
+            >
+              {description.label}
+            </p>
+            <ul className="social-elements">
+              {socialLogos.map((element) => (
+                <li key={element.id} className="social-element">
+                  <Link href={element.href}>
+                    {element.image && (
+                      <StrapiImage
+                        src={element.image?.url}
+                        alt={element.label.label}
+                        width={25}
+                        height={25}
+                      />
+                    )}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
       <div className="bottomFooter">
-        <ul>
+        <ul className="social-elements">
           {links.map((item) => (
             <Link
               key={item.id}
               href={item.href}
-              className={`text-${item.label?.color} textWeight-${item.label?.fontWeight} text-smallTitle`}
+              className={`text-${item.label?.color} textWeight-${item.label?.fontWeight} text-normal`}
             >
               {item.label?.label}
             </Link>
